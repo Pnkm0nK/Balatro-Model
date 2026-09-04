2875d5643ce3d3da99048801582932ae6427f656
Mikita Zyhmantovich <nucklearduk0@gmail.com>
2026-09-04 11:07:29 +0200

    Fixes

--- Checks/straight.cpp
+++ Checks/straight.cpp
@@ -3,31 +3,30 @@
 #include <array>
 #include <vector>
 
-std::vector<std::array<const Card*, 5>> is_straight(
-    const std::array<std::vector<const Card*>, ALL_CARD_RANKS.size()>& hand,
-    std::size_t threshold = 5)
-{
-    std::vector<std::array<const Card*, 5>> result;
-    std::array<const Card*, 5> straight{};
-    std::size_t count = 0;
+std::vector<std::array<const Card *, 5>> find_straight(
+    const std::array<std::vector<const Card *>, ALL_CARD_RANKS.size()> &hand,
+    std::size_t threshold = 5) {
+  std::vector<std::array<const Card *, 5>> result;
+  std::array<const Card *, 5> straight{};
+  std::size_t count = 0;
 
-    // The last iteration closes a straight that ends at Ace.
-    for (size_t i = 0; i <= hand.size(); ++i) {
-        if (i < hand.size() && !hand[i].empty()) {
-            if (count < straight.size()) {
-                straight[count] = hand[i][0];
-            }
-            ++count;
-            continue;
-        }
-
-        if (count >= threshold) {
-            result.push_back(straight);
-        }
-
-        straight = {};
-        count = 0;
+  // The last iteration closes a straight that ends at Ace.
+  for (size_t i = 0; i <= hand.size(); ++i) {
+    if (i < hand.size() && !hand[i].empty()) {
+      if (count < straight.size()) {
+        straight[count] = hand[i][0];
+      }
+      ++count;
+      continue;
     }
 
-    return result;
+    if (count >= threshold) {
+      result.push_back(straight);
+    }
+
+    straight = {};
+    count = 0;
+  }
+
+  return result;
 }
--- checks.hpp
+++ checks.hpp
@@ -4,16 +4,18 @@
 #include <array>
 #include <vector>
 
-bool is_flush(
-    const std::array<std::vector<const Card *>, ALL_SUITS.size()> &hand,
-    int threshold = 5);
+std::vector<std::array<const Card *, 5>>
+find_flush(const std::array<std::vector<const Card *>, ALL_SUITS.size()> &hand,
+           int threshold = 5);
 
-bool is_straight(
-    const std::array<std::vector<const Card *>, ALL_CARD_RANKS.size()> &hand);
+std::vector<std::array<const Card *, 5>> find_straight(
+    const std::array<std::vector<const Card *>, ALL_CARD_RANKS.size()> &hand,
+    int threshold = 5);
 
 std::vector<std::array<const Card *, 2>> find_pairs(
     const std::array<std::vector<const Card *>, ALL_CARD_RANKS.size()> &hand);
-
+std::vector<std::array<const Card *, 3>> find_triples(
+    const std::array<std::vector<const Card *>, ALL_CARD_RANKS.size()> &hand);
 std::vector<std::array<const Card *, 5>>
 find_full_house(const std::vector<std::array<const Card *, 2>> &pairs,
                 const std::vector<std::array<const Card *, 3>> &triplets);
